5bdebc67a01c6adbb71e65e0a359390ebf5d5ad8
gayagenius <gayaflo95@gmail.com>
2019-04-03 13:22:47 +0300

    adding to this repository

---
+++ example2/calculator.py
@@ -0,0 +1,31 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+def subtract(d,e):
+    f=d-e
+    print(f)	
+
+def multiply(g,h):
+    i=g*h
+    print(i)
+
+def divide(k,l):
+    m=k/l
+    print(m) 
+
+def modulus(n,p):
+    q=n%p
+    print(q)  
+
+def whole_division(r,t):
+    v=r//t
+    print(v)
+
+def percentage(x,y):
+	z=(x/y)*100
+	print("{}%".format(z))
+
+def exponent(s,u):
+    w=s**u
+    print(w)	
