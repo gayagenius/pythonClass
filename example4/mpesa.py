d84d2a32f560c64155dedf5f28e3c4bb14d99e19
gayagenius <gayaflo95@gmail.com>
2019-04-23 15:17:35 +0300

    the mpesa file

--- example4/mpesa.py
+++ example4/mpesa.py
@@ -12,9 +12,11 @@ class Mpesa_Account:
 		print ("Dear {}, You have sucessfuly deposited Ksh{} to the phone_number {}.Your new balance is Ksh{}.".format(self.name,amount,self.phone_number,self.balance))
 
 	def withdraw(self,amount):
-		self.balance=self.balance-amount
-		print("Dear {},You have withdrawn Kshs{} from the number {} Your new balance is Kshs{}.".format(self.name,amount,self.phone_number,self.balance))
-
+		if (amount < self.balance):	
+			self.balance=self.balance-amount
+			print("Dear {},You have withdrawn Kshs{} from the number {} Your new balance is Kshs{}.".format(self.name,amount,self.phone_number,self.balance))
+        else:
+        	print("Failed.You do not have enough money in your account to withdraw Kshs{}.".format(amount))
 
 	def check_balance(self):
 		print("Dear Customer,Your Mpesa Balance is Kshs{}.".format(self.balance))
