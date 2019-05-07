0a751456c2a5ee895c8fc47cbbfffa2edd9cfa98
gayagenius <gayaflo95@gmail.com>
2019-05-07 19:17:06 +0300

    adding quiz2

---
+++ quiz2/quiz2.py
@@ -0,0 +1,59 @@
+
+
+def tens():
+	numbers_list=[1,2,3,4,5,6,7,8,9,]
+	for x in numbers_list:
+		y=x*10
+		print(y)
+
+def squares():
+	numbers_list =[2,4,6,8,10,12]
+	v=[x**2 for x in numbers_list]
+	print(v)
+
+def sorted():
+	a=[9,1,4,7,3]
+	b=[5,2,6,8,0]
+	c=a+b
+	c.sort()
+	print(c)
+
+def range_sum(n):
+	g=[]
+	for x in range(n,n+1)
+	g.append(x)
+	answer=sum(g)
+
+def largest(n):
+	print max(n)
+
+def smallest(n):
+	print min(n)
+
+def my_modulus():
+	t=dict()
+	f=range(10,20)
+	for x in f:
+		t[x]=x%3
+		return(t)
+
+def list_student
+
+	student1 = {"name":"Irene","balance":1000}
+	student2 = {"name":"Pauline","balance":2000}
+	student3 = {"name":"Naima","balance":3000}
+	student5 = {"name":"Rose","balance":1000}
+
+	for student in students:
+	sms="Hi{},your balance is {}".format(student["name"],student["balance"])
+	print(sms)
+
+	
+
+
+
+
+
+
+
+
